15c3d11a608a4c9be5aac9ce8708d5e356e7500f
unknown <nahidukil547@gmail.com>
2021-03-17 17:48:52 +0600

    update

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$('.owl-carousel').owlCarousel({
+$('.owl-carousel-1').owlCarousel({
     loop: true,
     margin: 10,
     autoplay:true,
@@ -14,6 +14,40 @@ $('.owl-carousel').owlCarousel({
         },
         1000: {
             items: 3
+        }
+    }
+})
+
+var typed = new Typed('.type', {
+    strings: [
+        'Mobile App Template', 
+        'User Friendly ',
+        'Use Your Business..',
+        'App system !! ',
+    ],
+    typeSpeed: 50,
+    smartBackspace: true,
+    backSpeed: 100,
+    shuffle: false,
+    startDelay: 0,
+    stringsElement: null,
+});
+$('.owl-carousel-2').owlCarousel({
+    loop: true,
+    margin: 40,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        400: {
+            items: 2
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 4
         }
     }
 })
