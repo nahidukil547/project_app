7deff239b063168460435ceb2dd3912d3a7289ac
unknown <nahidukil547@gmail.com>
2021-03-30 10:09:31 +0600

    update

--- js/app.js
+++ js/app.js
@@ -51,3 +51,35 @@ $('.owl-carousel-2').owlCarousel({
         }
     }
 })
+$('.owl-carousel-3').owlCarousel({
+    loop: true,
+    margin: 20,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+$('.owl-carousel-4').owlCarousel({
+    loop: true,
+    margin: 20,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
